bcce3a6ea6432c19610d017706e8affee7b4b389
John Fredrickson <jfredson@gmail.com>
2015-07-08 20:56:34 -0400

    first commit

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,10 @@
+ require "instagram"
+
+    Instagram.configure do |config|
+
+    config.client_id = "c695f5205e384d19980b91335953ec0e"
+
+    config.access_token = "1546337130.c695f52.09aa01731c374d3f9f2614d851dc1255"
+
+end
+
